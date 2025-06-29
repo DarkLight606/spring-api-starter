1920d510bc26b94da5a4298aeaf7a499c42c1c0b
Darklight606 <dhalitov911@ukr.net>
2025-06-29 19:09:22 +0300

    customizing response data (@Json)

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,9 +1,11 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Setter
@@ -11,5 +13,10 @@ import lombok.Setter;
 public class UserDto {
     private Long id;
     private String name;
+    //@JsonInclude(JsonInclude.Include.NON_NULL) - exclude null rows
+    //@JsonIgnore - ignore row
+    //@JsonProperty - assign new name
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
