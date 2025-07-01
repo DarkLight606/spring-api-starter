b4280af4f9ad22a695b60ef6d84f4c2bc198ef07
Denis <dhalitov911@gmail.com>
2025-07-01 14:18:39 +0300

    29. Exercise Building a CRUD API

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -4,13 +4,14 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.CategoryRepository;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -19,6 +20,7 @@ public class ProductController {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public List<ProductDto> getAllProducts(
@@ -28,8 +30,7 @@ public class ProductController {
 
         if (categoryId != null) {
             products = productRepository.findByCategoryId(categoryId);
-        }
-        else {
+        } else {
             products = productRepository.findAllWithCategory();
         }
 
@@ -45,5 +46,69 @@ public class ProductController {
         }
 
         return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productDto,
+            UriComponentsBuilder uriBuilder
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        productRepository.save(product);
+
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(product.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(productMapper.toDto(product));
+    }
+
+    /**
+     * param productDto
+     * param id
+     * return
+     * RequestBody ProductDto productDto не містить поле id, бо ми отримуємо його через
+     * PathVariable Long id, отже там null
+     * Якщо ми на рядку 93 оновимо продукт (updateProduct) з айді null, буде пізда
+     * тому ми в ProductMapper додаємо: @Mapping(target = "id", ignore = true)
+     * і не оновлюємо його
+     * Також га 95 рядку присвоюємо це айді, щоб повернути респонс
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @RequestBody ProductDto productDto,
+            @PathVariable Long id
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productMapper.updateProduct(productDto, product);
+        product.setCategory(category);
+
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        return ResponseEntity.ok(productDto);
+    }
+
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -4,10 +4,15 @@ import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(target = "categoryId", source = "category.id")
     ProductDto toDto(Product product);
+
+    Product toEntity(ProductDto productDto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateProduct(ProductDto productDto, @MappingTarget Product product);
 }
