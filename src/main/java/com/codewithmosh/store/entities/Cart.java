062aaabf38a8c65701402fdedb3716323a2e9f5f
Darklight606 <dhalitov911@ukr.net>
2025-07-06 19:17:51 +0300

    Remove product and clear the cart

--- src/main/java/com/codewithmosh/store/entities/Cart.java
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -23,12 +23,45 @@ public class Cart {
     @Column(name = "date_created", insertable = false, updatable = false)
     private Date dateCreated;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE, orphanRemoval = true, fetch = FetchType.EAGER)
     private Set<CartItem> items = new LinkedHashSet<>();
 
     public BigDecimal getTotalPrice() {
         return items.stream()
                 .map(CartItem::getTotalPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public CartItem getItem(Long productId) {
+        return items.stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public CartItem addItem(Product product) {
+        var cartItem = getItem(product.getId());
+        if (cartItem == null) {
+            var item = new CartItem();
+            item.setProduct(product);
+            item.setQuantity(1);
+            item.setCart(this);
+            items.add(item);
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }
+        return cartItem;
+    }
+
+    public void removeItem(Long productId) {
+        var cartItem = getItem(productId);
+        if (cartItem != null) {
+            items.remove(cartItem);
+            cartItem.setCart(null);
+        }
+    }
+
+    public void clearItems() {
+        items.clear();
     }
 }
