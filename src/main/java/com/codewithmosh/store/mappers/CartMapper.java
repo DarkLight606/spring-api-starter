7789f0bd82d3af8b7179625b488fdfa449f01147
Darklight606 <dhalitov911@gmail.com>
2025-07-06 17:28:25 +0300

    Updating cart item

--- src/main/java/com/codewithmosh/store/mappers/CartMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/CartMapper.java
@@ -2,10 +2,12 @@ package com.codewithmosh.store.mappers;
 
 import com.codewithmosh.store.dtos.CartDto;
 import com.codewithmosh.store.dtos.CartItemDto;
+import com.codewithmosh.store.dtos.UpdateItemRequest;
 import com.codewithmosh.store.entities.Cart;
 import com.codewithmosh.store.entities.CartItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
@@ -14,4 +16,6 @@ public interface CartMapper {
 
     @Mapping(expression = "java(cartItem.getTotalPrice())", target = "totalPrice")
     CartItemDto toDto(CartItem cartItem);
+
+    void updateCartItem(UpdateItemRequest request, @MappingTarget CartItem cartItem);
 }
