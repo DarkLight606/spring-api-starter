bfe60989d4bae7dbb2ea9c4416b62889ac191825
Darklight606 <dhalitov911@gmail.com>
2025-07-20 20:00:41 +0300

    54. Working on AuthManager

--- src/main/java/com/codewithmosh/store/handlers/AuthExceptionHandler.java
+++ src/main/java/com/codewithmosh/store/handlers/AuthExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.codewithmosh.store.handlers;
 
-import com.codewithmosh.store.exceptions.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,14 +10,7 @@ import java.util.Map;
 
 @ControllerAdvice
 public class AuthExceptionHandler {
-    @ExceptionHandler({UsernameNotFoundException.class})
-    public ResponseEntity<Map<String, String>> handleUserNotFound() {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                Map.of("error", "User not found")
-        );
-    }
-
-    @ExceptionHandler({UnauthorizedException.class})
+    @ExceptionHandler({BadCredentialsException.class})
     public ResponseEntity<Map<String, String>> handleUnauthorized() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
                 Map.of("error", "Unauthorized")
